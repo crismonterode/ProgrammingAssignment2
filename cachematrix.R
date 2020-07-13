c94d2b469c6e0d79d777c7518f7ed5b691ef0472
crismonterode <68236309+crismonterode@users.noreply.github.com>
2020-07-13 16:16:13 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,18 +2,20 @@
 
 ##makeCacheMatrix will create a matrix, then its inverse, and it will cache its inverse value.
 
+#set the value of the matrix
+#get the value of the matrix
+#set the value of the inverse
+#get the value of the inverse
+
+
 makeCacheMatrix <- function(x = matrix()) {
-  #set the value of the matrix
     i <- NULL
     set <- function(y) {
       x <<- y
       i <<- NULL
     }
-    #get the value of the matrix
     get <- function() x
-    #set the value of the inverse
     setinverse <- function(inverse) i <<- inverse
-    #get the value of the inverse
     getinverse <- function() i
     list(set = set,
          get = get,
